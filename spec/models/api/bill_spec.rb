b9f58d2af115c1cb6990f795d0941eb591eaa28e
isaac jarquin <isaacjarquin@isaacjaquinsAir.home>
2015-08-19 20:04:16 +0100

    test for api call to receive the json

--- spec/models/api/bill_spec.rb
+++ spec/models/api/bill_spec.rb
@@ -2,7 +2,16 @@ require './app/models/api/bill.rb'
 
 module API
   describe Bill do
-    
+    let(:uri) { 'http://safe-plains-5453.herokuapp.com/bill.json' }
+    let(:bill) { "{\n\"some\":{\n\"json\":\"string\"}}" }
+
+    before do
+      allow(Net::HTTP).to receive(:get)
+        .with(URI(uri)).and_return(bill)
+    end
+
+    it 'return the bill' do
+      expect(Bill.new.get).to eql(JSON.parse(bill))
+    end
   end
 end
-
