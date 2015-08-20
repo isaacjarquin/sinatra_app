fc96e39bc5811ef92e9044dfc11da71d8a0af942
isaac jarquin <isaacjarquin@isaacjaquinsAir.home>
2015-08-20 22:11:40 +0100

    passing adapter to the controller to render on the view

--- app/controllers/bills/show.rb
+++ app/controllers/bills/show.rb
@@ -1,4 +1,11 @@
 get '/bills/:id' do
-  erb :bill, locals: { greeting: API::Bill.new.get }
-  # erb :bill
+  erb :bill, locals: { greeting: bill_adapted }
+end
+
+def bill_adapted
+  BillAdapter.adapt(bill)
+end
+
+def bill
+  API::Bill.new.get
 end
